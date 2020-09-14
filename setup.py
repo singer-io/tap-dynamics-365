045aad5af746edc121c4be9615e8b7dcf3bda15a
Collin Simon <csimon@talend.com>
2020-09-14 14:57:14 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+#usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-dynamics-365",
+    version="0.0.1",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_dynamics_365"],
+    install_requires=[
+        'singer-python==5.9.0',
+    ],
+    extras_require={
+        'dev': [
+            'ipdb',
+            'pylint',
+            'nose'
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+    tap-dynamics-365=tap_dynamics_365:main
+    """,
+    packages=["tap_dynamics_365"],
+    package_data = {
+    },
+    include_package_data=True,
+)
